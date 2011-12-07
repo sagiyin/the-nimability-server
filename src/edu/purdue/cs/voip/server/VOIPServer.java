c98484299c81178783a4430ad759f2ca002be1a3
sagi.yin@gmail.com <sagi.yin@gmail.com@369a77f5-227d-2f8d-9e15-3cae6ae33d81>
2011-12-07 04:50:32 +0000

    Initial Server prototype

---
+++ src/edu/purdue/cs/voip/server/VOIPServer.java
@@ -0,0 +1,63 @@
+package edu.purdue.cs.voip.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VOIPServer {
+  ServerSocket serverSocket;
+  List<Client> listClient = new ArrayList<Client>();
+  final static int defaultPort= 8888;
+  
+  public void start(int port) {
+    try {
+      serverSocket = new ServerSocket(port);
+    } catch (IOException ioeport) {
+      System.out.format("Port %d already in use.\n", port);
+    }
+    
+    if (serverSocket.isBound()) {
+      try {
+        serverSocket = new ServerSocket();
+      } catch (IOException ioe) {
+        System.out.format("Failed to bind server socket to any available port.\n Exiting...\n");
+      }
+    }
+    
+    if (serverSocket.isBound()) {
+      while (true) {
+        try {
+          Socket slaveSocket = serverSocket.accept();
+          Client newClient = new Client(this, slaveSocket);
+          newClient.start();
+          listClient.add(newClient);
+        } catch (IOException e) {
+          System.out.format("Failed to accept client connection.\n");
+        }
+      }
+    }
+  }
+  
+  public synchronized void logout(Client c) {
+    listClient.remove(c);
+  }
+  
+  public List<Client> getClientList(Client c) {
+    List<Client> listClient = this.listClient;
+    listClient.remove(c);
+    return listClient;
+  }
+  
+  public void call(Client caller, Client callee) {
+    
+  }
+  
+  public static void main(String args[]) {
+    VOIPServer voipServer = new VOIPServer();
+    voipServer.start(defaultPort);
+  }
+  
+}
+
