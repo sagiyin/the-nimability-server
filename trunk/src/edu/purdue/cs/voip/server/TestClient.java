59032f94c891e728540c336f6b43a99244e68ead
sagi.yin@gmail.com <sagi.yin@gmail.com@369a77f5-227d-2f8d-9e15-3cae6ae33d81>
2011-12-08 02:51:14 +0000

    Use BufferedReader

--- trunk/src/edu/purdue/cs/voip/server/TestClient.java
+++ trunk/src/edu/purdue/cs/voip/server/TestClient.java
@@ -1,29 +1,30 @@
 package edu.purdue.cs.voip.server;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class TestClient {
   Socket socket;
-  BufferedReader in;
-  private DataOutputStream out;
-  private Scanner incoming;
+  private InputStream in;
+  private OutputStream out;
+  private BufferedReader incoming;
   private PrintStream outgoing;
 
   public TestClient(String host, int port) {
     try {
       socket = new Socket(host, port);
-      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      out = new DataOutputStream(socket.getOutputStream());
-      incoming = new Scanner(in);
+      in = socket.getInputStream();
+      out = socket.getOutputStream();
+      incoming =new BufferedReader(new InputStreamReader(in));
       outgoing = new PrintStream(out);
     } catch (UnknownHostException e) {
       System.out.format("Unknown Host: %s", host);
@@ -41,17 +42,23 @@ public class TestClient {
 
     outgoing.println(gson.toJson(request));
     outgoing.flush();
-    
-    while (true) {  
-      if (incoming.hasNextLine()) {
-        String jsonString = incoming.nextLine();
-        System.out.format("Received server response json:%s\n", jsonString);
-        ServerResponse response = gson.fromJson(jsonString.toString(), ServerResponse.class);
-        for (String s : response.getListOfClients()) {
-          System.out.println(s);
+    try {
+      while (true) {
+        String jsonString;
+        while ((jsonString = incoming.readLine()) != null) {
+          System.out.format("Received server response json:%s\n", jsonString);
+          ServerResponse response = gson.fromJson(jsonString.toString(), ServerResponse.class);
+          for (String s : response.getListOfClients()) {
+            System.out.println(s);
+          }
         }
       }
-
+    } catch (JsonSyntaxException e) {
+      System.out.println("Failed to parse JSON response from server.");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("Failed to read data from socket.");
+      e.printStackTrace();
     }
 
   }
