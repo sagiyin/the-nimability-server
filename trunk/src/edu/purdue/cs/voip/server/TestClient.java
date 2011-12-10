0901a9904638896a9d0fcf2058483e513d438b19
yezhuen@gmail.com <yezhuen@gmail.com@369a77f5-227d-2f8d-9e15-3cae6ae33d81>
2011-12-10 14:49:12 +0000

    Final Server

--- trunk/src/edu/purdue/cs/voip/server/TestClient.java
+++ trunk/src/edu/purdue/cs/voip/server/TestClient.java
@@ -1,30 +1,30 @@
 package edu.purdue.cs.voip.server;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class TestClient {
 	Socket socket;
-	BufferedReader in;
-	private DataOutputStream out;
-	private Scanner incoming;
+	private InputStream in;
+	private OutputStream out;
+	private BufferedReader incoming;
 	private PrintStream outgoing;
 
 	public TestClient(String host, int port) {
 		try {
 			socket = new Socket(host, port);
-			in = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
-			out = new DataOutputStream(socket.getOutputStream());
-			incoming = new Scanner(in);
+			in = socket.getInputStream();
+			out = socket.getOutputStream();
+			incoming = new BufferedReader(new InputStreamReader(in));
 			outgoing = new PrintStream(out);
 		} catch (UnknownHostException e) {
 			System.out.format("Unknown Host: %s", host);
@@ -46,31 +46,39 @@ public class TestClient {
 		//outgoing.flush();
 
 		while (true) {
-			if (incoming.hasNextLine()) {
-				String jsonString = incoming.nextLine();
-				System.out.format("Received server response json:%s\n",
-						jsonString);
-				ServerResponse response = gson.fromJson(jsonString.toString(),
-						ServerResponse.class);
-				if (response.getResponseType().equals(VOIPConstant.RESPONSE_LIST_ALL)) {
-					for (String s : response.getListOfClients()) {
-						System.out.println(s);
+			String jsonString;
+			try {
+				while ((jsonString = incoming.readLine()) != null) {
+					System.out.format("Received server response json:%s\n",
+							jsonString);
+					ServerResponse response = gson.fromJson(jsonString.toString(),
+							ServerResponse.class);
+					if (response.getResponseType().equals(VOIPConstant.RESPONSE_LIST_ALL)) {
+						for (String s : response.getListOfClients()) {
+							System.out.println(s);
+						}
+					}else if(response.getResponseType().equals(VOIPConstant.OP_REACH_CALLEE)){
+						String callerIp = response.getRequestTarget();
+						ClientRequest requestTmp = new ClientRequest();
+						System.out.println("call accepted");
+						requestTmp.setRequestType(VOIPConstant.OP_REQUEST_ACCEPT);
+						requestTmp.setRequestTarget(callerIp);
+						outgoing.println(gson.toJson(requestTmp));
+						outgoing.flush();
+					} else if(response.getResponseType().equals( VOIPConstant.OP_RESPONSE_CALL)){
+						
+						System.out.println(response.getCalleeStatus());
+					}else if(response.getResponseType().equals( VOIPConstant.OP_REACH_SENDMESSAGE)){
+						
+						System.out.println(response.getReachMessage()+"\nFROM"+response.getRequestTarget());
 					}
-				}else if(response.getResponseType().equals(VOIPConstant.OP_REACH_CALLEE)){
-					String callerIp = response.getRequestTarget();
-					ClientRequest requestTmp = new ClientRequest();
-					System.out.println("call accepted");
-					requestTmp.setRequestType(VOIPConstant.OP_REQUEST_ACCEPT);
-					requestTmp.setRequestTarget(callerIp);
-					outgoing.println(gson.toJson(requestTmp));
-					outgoing.flush();
-				} else if(response.getResponseType().equals( VOIPConstant.OP_RESPONSE_CALL)){
-					
-					System.out.println(response.getCalleeStatus());
-				}else if(response.getResponseType().equals( VOIPConstant.OP_REACH_SENDMESSAGE)){
-					
-					System.out.println(response.getReachMessage()+"\nFROM"+response.getRequestTarget());
 				}
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 
 		}
--- trunk/src/edu/purdue/cs/voip/server/TestClient2.java
+++ trunk/src/edu/purdue/cs/voip/server/TestClient2.java
@@ -1,30 +1,30 @@
 package edu.purdue.cs.voip.server;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class TestClient2 {
 	Socket socket;
-	BufferedReader in;
-	private DataOutputStream out;
-	private Scanner incoming;
+	private InputStream in;
+	private OutputStream out;
+	private BufferedReader incoming;
 	private PrintStream outgoing;
 
 	public TestClient2(String host, int port) {
 		try {
 			socket = new Socket(host, port);
-			in = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
-			out = new DataOutputStream(socket.getOutputStream());
-			incoming = new Scanner(in);
+			in = socket.getInputStream();
+			out = socket.getOutputStream();
+			incoming = new BufferedReader(new InputStreamReader(in));
 			outgoing = new PrintStream(out);
 		} catch (UnknownHostException e) {
 			System.out.format("Unknown Host: %s", host);
@@ -46,41 +46,50 @@ public class TestClient2 {
 		outgoing.flush();
 
 		while (true) {
-			if (incoming.hasNextLine()) {
-				String jsonString = incoming.nextLine();
-				System.out.format("Received server response json:%s\n",
-						jsonString);
-				ServerResponse response = gson.fromJson(jsonString.toString(),
-						ServerResponse.class);
-				if (response.getResponseType().equals(VOIPConstant.RESPONSE_LIST_ALL)) {
-					System.out.println("Entered LIST_ALL");
-					for (String s : response.getListOfClients()) {
-						System.out.println(s);
-					}
-				}else if(response.getResponseType().equals(VOIPConstant.OP_REACH_CALLEE)){
-					System.out.println("Entered REACH_CALLEE");
-					String callerIp = response.getRequestTarget();
-					ClientRequest requestTmp = new ClientRequest();
-					requestTmp.setRequestType(VOIPConstant.OP_REQUEST_DECLINE);
-					requestTmp.setRequestTarget(callerIp);
-					outgoing.println(gson.toJson(requestTmp));
-					outgoing.flush();
-				} else if(response.getResponseType().equals( VOIPConstant.OP_RESPONSE_CALL)){
-					System.out.println("Entered OP_RESPONSE_CALL");
-			/*		if(response.getCalleeStatus()==(VOIPConstant.CALLEE_STATUS_BUSY)) System.out.println("CALLEE IS BUSY");
-					else if(response.getCalleeStatus()==(VOIPConstant.CALLEE_STATUS_DECLINE)) System.out.println("CALLEE DECLINE");*/
-					 if( ((Integer)response.getCalleeStatus()).equals(VOIPConstant.CALLEE_STATUS_READY)) {
-						System.out.println("CALLEE READY, connecting"); 
+			String jsonString;
+			try {
+				while ((jsonString = incoming.readLine()) != null) {
+				
+					System.out.format("Received server response json:%s\n",
+							jsonString);
+					ServerResponse response = gson.fromJson(jsonString.toString(),
+							ServerResponse.class);
+					if (response.getResponseType().equals(VOIPConstant.RESPONSE_LIST_ALL)) {
+						System.out.println("Entered LIST_ALL");
+						for (String s : response.getListOfClients()) {
+							System.out.println(s);
+						}
+					}else if(response.getResponseType().equals(VOIPConstant.OP_REACH_CALLEE)){
+						System.out.println("Entered REACH_CALLEE");
 						String callerIp = response.getRequestTarget();
 						ClientRequest requestTmp = new ClientRequest();
-						requestTmp.setRequestType(VOIPConstant.OP_REQUEST_CONNECTED);
+						requestTmp.setRequestType(VOIPConstant.OP_REQUEST_DECLINE);
 						requestTmp.setRequestTarget(callerIp);
 						outgoing.println(gson.toJson(requestTmp));
 						outgoing.flush();
+					} else if(response.getResponseType().equals( VOIPConstant.OP_RESPONSE_CALL)){
+						System.out.println("Entered OP_RESPONSE_CALL");
+				/*		if(response.getCalleeStatus()==(VOIPConstant.CALLEE_STATUS_BUSY)) System.out.println("CALLEE IS BUSY");
+						else if(response.getCalleeStatus()==(VOIPConstant.CALLEE_STATUS_DECLINE)) System.out.println("CALLEE DECLINE");*/
+						 if( ((Integer)response.getCalleeStatus()).equals(VOIPConstant.CALLEE_STATUS_READY)) {
+							System.out.println("CALLEE READY, connecting"); 
+							String callerIp = response.getRequestTarget();
+							ClientRequest requestTmp = new ClientRequest();
+							requestTmp.setRequestType(VOIPConstant.OP_REQUEST_CONNECTED);
+							requestTmp.setRequestTarget(callerIp);
+							outgoing.println(gson.toJson(requestTmp));
+							outgoing.flush();
+						}
+						 else{System.out.println("FUCK");}
+						/*else if(response.getCalleeStatus()==(VOIPConstant.CALLEE_STATUS_NOT_EXIST)) System.out.println("CALLEE NOT EXIST");*/
 					}
-					 else{System.out.println("FUCK");}
-					/*else if(response.getCalleeStatus()==(VOIPConstant.CALLEE_STATUS_NOT_EXIST)) System.out.println("CALLEE NOT EXIST");*/
 				}
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 
 		}
